a2c66889f4ebc8b4971efe7a2abf16cd6eceb12d
Cuchu Seré <69125170+Cuchu59@users.noreply.github.com>
2024-05-31 10:36:43 -0400

    Update dicts.py

--- dicts.py
+++ dicts.py
@@ -7,16 +7,19 @@ def create_inventory(items):
 def add_items(map_inv, list_items, dir = 1):
     new_inv = dict()
     
+    if map_inv == {}:
+        return create_inventory(list_items)
+    
     for key in map_inv.keys():     
         for it in list_items:
+            new_inv[key] = map_inv[key]
+
             new_inv[it] = dir * list_items.count(it) + map_inv[it] if it in map_inv else list_items.count(it)
                 
             if new_inv[it] < 0:
                 new_inv[it] = 0
-
-            new_inv[key] = map_inv[key]
-                
     return new_inv
+
 
 def decrement_items(inventory, items):
     return add_items(inventory,items,-1)
@@ -29,5 +32,9 @@ def remove_item(inventory, item):
 def list_inventory(dict_inv):
     new_list = []
     for item in dict_inv.keys():
+        if dict_inv[item] > 0:
+            new_list.append((item, dict_inv[item]))
+
+    return new_list
         new_list.append((item, dict_inv[item]))
     return new_list
